fa90164fd832a0ac01bb9af8e982cd52bfbba995
Greedysky <greedysky@163.com>
2025-11-07 15:18:47 +0800

    feat: Support for qmmp 0.x Qt4

--- decoderadlmidifactory.cpp
+++ decoderadlmidifactory.cpp
@@ -99,3 +99,8 @@ QString DecoderAdlMidiFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(adlmidi, DecoderAdlMidiFactory)
+#endif
